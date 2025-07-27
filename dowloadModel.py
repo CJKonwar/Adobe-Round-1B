17b78b0a059ef6706e73a662a5be9607d739d44d
Champakjyoti Konwar <153445515+CJKonwar@users.noreply.github.com>
2025-07-27 20:19:36 +0530

    Updated models

--- dowloadModel.py
+++ dowloadModel.py
@@ -1,3 +1,5 @@
 from sentence_transformers import SentenceTransformer
 model = SentenceTransformer('cross-encoder/ms-marco-MiniLM-L6-v2')
+model = SentenceTransformer('BAAI/bge-base-en-v1.5')
 model.save('cross-encoder/ms-marco-MiniLM-L6-v2')
+model.save('BAAI/bge-base-en-v1.5')
